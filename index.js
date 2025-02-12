466da74da4b3b4d11f7b8a39b9511ec44dd0a645
sajan <sajandordie@gmail.com>
2025-02-12 14:44:59 +0530

    change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World sajan!')
+  res.send('Hello World David!')
 })
 
 app.get('/status', (req, res) => {
